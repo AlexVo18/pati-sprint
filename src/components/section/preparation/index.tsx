e2a5c476534159564bc327db7c61dae9161e4650
AlexVo18 <trivm182003@gmail.com>
2026-01-31 22:10:49 +0700

    feat: add preparation section

--- src/components/section/preparation/index.tsx
+++ src/components/section/preparation/index.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Container from "@/components/common/container";
+import { steps } from "./mock-data";
+import Card from "./card";
 
 const Preparation = () => {
-  return <div>Preparation</div>;
+  return (
+    <Container id="preparation" isLargeScreen className="md:py-26 py-14">
+      <div className="flex flex-col md:gap-14 gap-8">
+        <h1 className="font-louzie md:text-[52px] text-[40px] leading-11 text-center">
+          How to Enjoy
+        </h1>
+        <div className="grid md:grid-cols-3 md:gap-4 gap-8">
+          {steps.map((step) => (
+            <Card step={step} key={step.title} />
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Preparation;
